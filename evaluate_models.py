78c8f77d1c04bde57f3d4f470263603d4fefd231
Konstantin K <zal1358@gmail.com>
2017-12-13 23:13:32 +0200

    graphix added

--- evaluate_models.py
+++ evaluate_models.py
@@ -1,31 +1,77 @@
 import numpy as np
 from keras.models import load_model
-import cv2
-from utils import get_face
 
-model_number = '45m15ev3_it7'
+db = "imdb"
+data_path = f"data/{db}_arrays_cropped_faces/"
+model_number = '45m15ev3_it'
 
-def gender(image_num, cat):
-    image_name = f"images/{cat}/{image_num}.jpg"
-    cropped_face = get_face(image_name)
-    resized = cv2.resize(cropped_face, (227,227), interpolation = cv2.INTER_LINEAR)
-#    cv2.imshow('image_num', cropped_face)
-    test_image = np.expand_dims(resized, axis = 0)
-    predict = classifier.predict(test_image)
-    if predict[0,0] > predict[0,1]:
-        detected_gender = 'woman'
-    else:
-        detected_gender = 'man'
-    return detected_gender, round(predict[0,0]*100,4), round(predict[0,1]*100,4)
+array_num = 0
+print(f'loading {data_path}images_{db}_{array_num}.npy')
+X = np.load(f'{data_path}images_{db}_{array_num}.npy')
+print(f'loading {data_path}gender_{db}_{array_num}.npy')
+y = np.load(f'{data_path}gender_{db}_{array_num}.npy')
 
-classifier = load_model(f'models/imdb_model_gender_{model_number}.h5')
+eval_loss = []
+eval_perc = []
 
-print ("females")
-for i in range(1,11):
-    detected_gender, woman_score, man_score = gender(i, 'f')
-    print (f"image {i} is a {detected_gender}, score is {woman_score} / {man_score}")
+for i in range(46):
+    model = load_model(f'models/imdb_model_gender_{model_number}{i}.h5')
+    evaluated = model.evaluate(X, y, batch_size=50)
+    print(evaluated)
+    eval_loss.append(evaluated[0])
+    eval_perc.append(evaluated[1])
+
+print(eval_loss)
+eval_loss.plot(title="loss")
+print(eval_perc)
+eval_perc.plot(title="perc")
+
+import matplotlib.pyplot as plt
+plt.plot(eval_loss)
+plt.plot(eval_perc)
+
+eval_loss1 = np.array(eval_loss)
+eval_perc1 = np.array(eval_perc)
+np.save('loss', np.array(eval_loss))
+np.save('perc', np.array(eval_perc))
     
-print ("males")
-for i in range(1,11):
-    detected_gender, woman_score, man_score = gender(i, 'm')
-    print (f"image {i} is a {detected_gender}, score is {woman_score} / {man_score}")
+#y_hat = model.predict(X)
+#ans = 0
+#for i in range(len(y)):
+#    if y_hat[i,0] > y_hat[i,1]:
+#        detected_gender = [1,0]
+#    else:
+#        detected_gender = [0,1]
+#    if list(y[i]) == detected_gender:
+#        ans+=1
+#        print (f"{i} detected correct")
+#    else:
+#        print (f"{i} detected INcorrect")
+#print (ans/len(y)*100)
+#        
+
+#
+#for array_num in range(0, arrays_number+1):
+#    print(f'loading {data_path}images_{db}_{array_num}.npy')
+#    X = np.load(f'{data_path}images_{db}_{array_num}.npy')
+#    print(f'loading {data_path}gender_{db}_{array_num}.npy')
+#    y = np.load(f'{data_path}gender_{db}_{array_num}.npy')
+#    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#    model.fit(X_train, y_train, 
+#              epochs=epochs_number,
+#              batch_size=50,
+#              validation_data=(X_test, y_test))
+#    print (f'saving model models/{db}_model_gender_{model_number}_it{array_num}.h5')
+#    model.save(f'models/{db}_model_gender_{model_number}_it{array_num}.h5')
+#
+#
+#
+#print ("females")
+#for i in range(1,11):
+#    detected_gender, woman_score, man_score = gender(i, 'f')
+#    print (f"image {i} is a {detected_gender}, score is {woman_score} / {man_score}")
+#    
+#print ("males")
+#for i in range(1,11):
+#    detected_gender, woman_score, man_score = gender(i, 'm')
+#    print (f"image {i} is a {detected_gender}, score is {woman_score} / {man_score}")
